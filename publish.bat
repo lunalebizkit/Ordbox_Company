b5373ae13df41634eb5b778749dc36b1bae70131
Ale Luna <lunalebizkit@gmail.com>
2026-09-21 09:30:37 -0300

    fix batch

--- publish.bat
+++ publish.bat
@@ -1,15 +1,15 @@
- .\publish.bat $(git rev-parse HEAD)
+@REM   .\publish.bat $(git rev-parse HEAD)
  cd BE
  
  IF NOT "%2" == "FE" (
- docker build --no-cache -f Dockerfile --tag lunale/ordboxcompany:api-%1 --tag lunale/ordboxcompany:api-latest .
+ docker build -f Dockerfile --tag lunale/ordboxcompany:api-%1 --tag lunale/ordboxcompany:api-latest .
  docker push lunale/ordboxcompany:api-%1
- docker push lunale/ordboxcompany:api-latest
+  docker push lunale/ordboxcompany:api-latest
  )
 
-  cd ..\front
+ cd ..\FE
  if not "%2" == "BE" (
-  docker build -f dockerfile --tag lunale/ordboxcompany:front-%1 --tag lunale/ordboxcompany:front-latest .
+ docker build -f Dockerfile --tag lunale/ordboxcompany:front-%1 --tag lunale/ordboxcompany:front-latest .
  docker push lunale/ordboxcompany:front-%1
  docker push lunale/ordboxcompany:front-latest
   )
